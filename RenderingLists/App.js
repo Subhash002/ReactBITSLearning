e17a8125f42e454388035daac2d588083757edee
Subhash Kandhway <kandhway002subhash@gmail.com>
2023-03-31 08:12:35 +0530

    Update App.js

--- RenderingLists/App.js
+++ RenderingLists/App.js
@@ -20,3 +20,25 @@ export default function List() {
   );
   return <ul>{listItems}</ul>;
 }
+
+import { recipes } from './data.js';
+
+export default function RecipeList() {
+  return (
+    <div>
+      <h1>Recipes</h1>
+      {recipes.map(recipe=>(
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <ul>
+          {recipe.ingredients.map(ingredient =>
+              <li key={ingredient}>
+                {ingredient}
+              </li>
+            )}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
