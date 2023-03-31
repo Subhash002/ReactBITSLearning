2d3332d7c866ac66b52846139e7ec88be9197801
Subhash Kandhway <kandhway002subhash@gmail.com>
2023-03-31 08:43:09 +0530

    Update App.js

--- RenderingLists/App.js
+++ RenderingLists/App.js
@@ -71,3 +71,30 @@ const Recipe=(props)=>{
     </div>
   );
 }
+
+//
+const poem = {
+  lines: [
+    'I write, erase, rewrite',
+    'Erase again, and then',
+    'A poppy blooms.'
+  ]
+};
+
+export default function Poem() {
+  return (
+    <article>
+      
+      {poem.lines.map((line, i) =>
+      <fragment key={i}>
+        <p key={i}>
+          {i>0 && <hr/>}
+          {line}
+        </p>
+        
+      </fragment>             
+      )}
+    </article>
+  );
+}
+
